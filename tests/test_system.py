def2d8620d34ae10b2b3d32090a5adece8dca6ba
Henry Webster <hwebster@mail.depaul.edu>
2019-10-23 02:17:22 -0400

    support for render system

---
+++ tests/test_system.py
@@ -0,0 +1,27 @@
+"""
+Unit tests for systems.
+"""
+
+from turpyno.component import (
+    IdentifierFactory,
+    RendererFactory,
+    IdentifierContext,
+    RendererContext,
+)
+from turpyno.entity import EntityFactory
+from turpyno.system import RenderSystem
+
+
+class TestSystem:
+    def test_render_system(self) -> None:
+
+        identifier_factory = IdentifierFactory()
+        identifier = identifier_factory.create(IdentifierContext("entity a"))
+        renderer_factory = RendererFactory()
+        renderer = renderer_factory.create(RendererContext(None, 50))
+        entity_factory = EntityFactory()
+        entity = entity_factory.create([identifier, renderer])
+
+        system = RenderSystem()
+        assert system.handles(entity)
+        system.register(entity)
