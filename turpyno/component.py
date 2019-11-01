a0543428c8fa1dc21b56022268e0fa56d346e9e4
Henry Webster <hwebster@mail.depaul.edu>
2019-10-31 21:29:55 -0400

    get rid of useless type id

--- turpyno/component.py
+++ turpyno/component.py
@@ -2,33 +2,17 @@
 Unit tests for the component class.
 """
 
-from enum import Enum
 from turpyno.generator import unique_id
-
-
-class TypeId(Enum):
-    """Types enum for components."""
-
-    IDENTIFIER = (1,)
-    RENDERER = 2
 
 
 class Component:  # pylint: disable=too-few-public-methods
     """Super class for all components."""
-
-    def __init__(self, type_id: TypeId):
-        self.type = type_id
-
-    def cid(self) -> TypeId:
-        """Return the unique identifier for this component type."""
-        return self.type
 
 
 class Identifier(Component):
     """Component that identifies."""
 
     def __init__(self, eid: int, name: str) -> None:
-        super().__init__(TypeId.IDENTIFIER)
         self._eid = eid
         self._name = name
         self._alive = False
