bdef88f49d1437353125217cd066bb2b3ffea804
Henry Webster <henry@hwebs.info>
2019-11-02 17:19:30 -0400

    use locator in render system

--- tests/test_system.py
+++ tests/test_system.py
@@ -2,7 +2,7 @@
 Unit tests for systems.
 """
 
-from turpyno.component import IdentifierFactory
+from turpyno.component import IdentifierFactory, LocatorFactory
 from turpyno.entity import EntityFactory
 from turpyno.renderer import RendererFactory
 from turpyno.system import RenderSystem
@@ -15,8 +15,10 @@ class TestSystem:
         identifier = identifier_factory.create("entity a")
         renderer_factory = RendererFactory(None, None)
         renderer = renderer_factory.create_rectangle()
+        locator_factory = LocatorFactory()
+        locator = locator_factory.create()
         entity_factory = EntityFactory()
-        entity = entity_factory.create([identifier, renderer])
+        entity = entity_factory.create([identifier, renderer, locator])
 
         system = RenderSystem()
         assert system.handles(entity)
--- turpyno/system.py
+++ turpyno/system.py
@@ -4,7 +4,7 @@ Class for the game systems.
 
 from typing import List
 
-from turpyno.component import Identifier
+from turpyno.component import Identifier, Locator
 from turpyno.renderer import Renderer
 from turpyno.entity import Entity
 
@@ -18,7 +18,7 @@ class RenderSystem:
     @classmethod
     def handles(cls, entity: Entity) -> bool:
         """Return true if an entity has all the needed component types."""
-        return entity.has(Identifier) and entity.has(Renderer)
+        return entity.has(Identifier) and entity.has(Renderer) and entity.has(Locator)
 
     def register(self, entity: Entity) -> None:
         """Put this entity in the processing list."""
@@ -27,4 +27,7 @@ class RenderSystem:
     def render(self) -> None:
         """Render to the screen."""
         for entity in self._entities:
-            entity.get(Renderer).render()
+            translation = entity.get(Locator).translation()
+            renderer = entity.get(Renderer)
+            renderer.translate(translation)
+            renderer.render()
