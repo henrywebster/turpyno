886c4a5b6fad4c2bced558a1ebcf08d893aa2db5
Henry Webster <henry@hwebs.info>
2019-11-03 09:25:57 -0500

    basic collision

--- turpyno/mover.py
+++ turpyno/mover.py
@@ -2,17 +2,19 @@
 Class for moving entities.
 """
 
+from typing import List, Optional
 import numpy as np  # type: ignore
-from turpyno.component import Component
+from turpyno.component import Component, Collider
 
 
 class Mover(Component):
     """This component handles movement."""
 
-    def __init__(self, offset: np.array) -> None:
+    def __init__(self, offset: np.array, obstructs: List[Collider]) -> None:
         self._target = np.array([0, 0, 0], dtype=np.float32)
         self._translation = offset
         self._velocity = 1.0
+        self._obstructs = obstructs
 
     def target(self, aim: np.array) -> None:
         """Set where to aim the mover."""
@@ -21,6 +23,16 @@ class Mover(Component):
     def velocitize(self, velocity: float) -> None:
         """Set how much velocity."""
         self._velocity = velocity
+
+    def obstructed(self) -> bool:
+        """Check if there is something in the way."""
+        candidate = np.add(self._translation, np.multiply(self._target, self._velocity))
+        for collider in self._obstructs:  # multithread!
+            print(collider)
+            print(candidate[0])
+            if collider.query_point(candidate[0], candidate[1]):
+                return True
+        return False
 
     def move(self) -> np.array:  # pylint: disable=no-self-use
         """Set a new translation based on target and velocity."""
@@ -34,10 +46,12 @@ class MoverFactory:  # pylint: disable=too-few-public-methods
     """Factory for mover components."""
 
     def create(  # pylint: disable=no-self-use
-        self,  # pylint: disable=bad-continuation
-        offset: np.array = np.array(  # pylint: disable=bad-continuation
-            [0, 0, 0], dtype=np.float32  # pylint: disable=bad-continuation
-        ),  # pylint: disable=bad-continuation
+        self,
+        offset: np.array = np.array([0, 0, 0], dtype=np.float32),
+        obstructs: Optional[List[Collider]] = None,
     ) -> Mover:
         """Create a new mover."""
-        return Mover(offset)
+        if obstructs is None:
+            print("nooooo")
+            return Mover(offset, [])
+        return Mover(offset, obstructs)
