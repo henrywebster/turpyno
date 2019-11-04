286459a75673c81c73440a51847c92bb280b52e4
Henry Webster <hwebster@mail.depaul.edu>
2019-11-03 19:28:02 -0500

    componentize menu items

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -7,6 +7,7 @@ from pytest import fixture  # type: ignore
 from pygame import Surface  # type: ignore
 from pygame.font import Font, get_default_font, init, quit  # type: ignore
 from turpyno.menu import Menu, MenuItem
+from turpyno.renderer import RendererFactory
 
 
 @fixture(scope="module", autouse=True)
@@ -20,9 +21,11 @@ class TestMenu:
     def test_menu_item(self) -> None:
         font = Font(get_default_font(), 12)
         surface = Surface((1, 1))
+        renderer_factory = RendererFactory(surface)
+        renderer = renderer_factory.create_text("test", font)
         l: List[int] = []
         action = lambda: l.append(1)  # noqa: E731
-        item = MenuItem("test", font, surface, action)
+        item = MenuItem(renderer, action)
         assert 0 == len(l)
 
         item.action()
@@ -31,9 +34,11 @@ class TestMenu:
     def test_menu_focus(self) -> None:
         font = Font(get_default_font(), 12)
         surface = Surface((1, 1))
+        renderer_factory = RendererFactory(surface)
+        renderer = renderer_factory.create_text("test", font)
         l: List[int] = []
         action = lambda: l.append(1)  # noqa: E731
-        item = MenuItem("test", font, surface, action)
+        item = MenuItem(renderer, action)
         menu = Menu([item])
         assert [] == l
 
@@ -43,11 +48,14 @@ class TestMenu:
     def test_menu_next(self) -> None:
         font = Font(get_default_font(), 12)
         surface = Surface((1, 1))
+        renderer_factory = RendererFactory(surface)
+        renderer_a = renderer_factory.create_text("a", font)
+        renderer_b = renderer_factory.create_text("b", font)
         l: List[int] = []
         action_a = lambda: l.append(1)  # noqa: E731
         action_b = lambda: l.append(2)  # noqa: E731
-        item_a = MenuItem("a", font, surface, action_a)
-        item_b = MenuItem("b", font, surface, action_b)
+        item_a = MenuItem(renderer_a, action_a)
+        item_b = MenuItem(renderer_b, action_b)
         menu = Menu([item_a, item_b])
         assert [] == l
 
@@ -61,11 +69,14 @@ class TestMenu:
     def test_menu_previous(self) -> None:
         font = Font(get_default_font(), 12)
         surface = Surface((1, 1))
+        renderer_factory = RendererFactory(surface)
+        renderer_a = renderer_factory.create_text("a", font)
+        renderer_b = renderer_factory.create_text("b", font)
         l: List[int] = []
         action_a = lambda: l.append(1)  # noqa: E731
         action_b = lambda: l.append(2)  # noqa: E731
-        item_a = MenuItem("a", font, surface, action_a)
-        item_b = MenuItem("b", font, surface, action_b)
+        item_a = MenuItem(renderer_a, action_a)
+        item_b = MenuItem(renderer_b, action_b)
         menu = Menu([item_a, item_b])
         assert [] == l
 
@@ -83,11 +94,14 @@ class TestMenu:
     def test_menu_next_wrap(self) -> None:
         font = Font(get_default_font(), 12)
         surface = Surface((1, 1))
+        renderer_factory = RendererFactory(surface)
+        renderer_a = renderer_factory.create_text("a", font)
+        renderer_b = renderer_factory.create_text("b", font)
         l: List[int] = []
         action_a = lambda: l.append(1)  # noqa: E731
         action_b = lambda: l.append(2)  # noqa: E731
-        item_a = MenuItem("a", font, surface, action_a)
-        item_b = MenuItem("b", font, surface, action_b)
+        item_a = MenuItem(renderer_a, action_a)
+        item_b = MenuItem(renderer_b, action_b)
         menu = Menu([item_a, item_b])
         assert [] == l
 
@@ -105,11 +119,14 @@ class TestMenu:
     def test_menu_previous_wrap(self) -> None:
         font = Font(get_default_font(), 12)
         surface = Surface((1, 1))
+        renderer_factory = RendererFactory(surface)
+        renderer_a = renderer_factory.create_text("a", font)
+        renderer_b = renderer_factory.create_text("b", font)
         l: List[int] = []
         action_a = lambda: l.append(1)  # noqa: E731
         action_b = lambda: l.append(2)  # noqa: E731
-        item_a = MenuItem("a", font, surface, action_a)
-        item_b = MenuItem("b", font, surface, action_b)
+        item_a = MenuItem(renderer_a, action_a)
+        item_b = MenuItem(renderer_b, action_b)
         menu = Menu([item_a, item_b])
         assert [] == l
 
--- turpyno/system.py
+++ turpyno/system.py
@@ -7,32 +7,19 @@ from pygame import Surface  # type: ignore
 from turpyno.component import Identifier, Locator
 from turpyno.renderer import Renderer, RendererFactory
 from turpyno.entity import Entity
-from turpyno.menu import MenuItemFactory, MenuItem
 
 
 class RenderSystem:
     """Class for rendering system."""
 
-    def __init__(
-        self, surface: Surface, factory: RendererFactory, menu_factory: MenuItemFactory
-    ) -> None:
+    def __init__(self, surface: Surface, factory: RendererFactory) -> None:
         self._entities: List[Entity] = []
         self._factory = factory
-        self._menu_factory = menu_factory
         self._surface = surface
-        self._menus: List[MenuItem] = []
 
     def renderer_factory(self) -> RendererFactory:
         """Return a renderer factory that will draw when this system renders."""
         return self._factory
-
-    def menu_factory(self) -> MenuItemFactory:
-        """Return a menu factory that will draw when this system renderers."""
-        return self._menu_factory
-
-    def menus(self, menus: List[MenuItem]) -> None:
-        """Temporary method for storing menu items."""
-        self._menus = menus
 
     @classmethod
     def handles(cls, entity: Entity) -> bool:
@@ -50,8 +37,6 @@ class RenderSystem:
             renderer = entity.get(Renderer)
             renderer.translate(translation)
             renderer.render()
-        for item in self._menus:
-            item.render()
         return self._surface
 
 
@@ -59,10 +44,7 @@ class RenderSystemFactory:  # pylint: disable=too-few-public-methods
     """Class for creating a render system."""
 
     def create(  # pylint: disable=no-self-use
-        self,
-        surface: Surface,
-        factory: Callable[[Surface], RendererFactory],
-        menu_factory: Callable[[Surface], MenuItemFactory],
+        self, surface: Surface, factory: Callable[[Surface], RendererFactory]
     ) -> RenderSystem:
         """Creates a render system."""
-        return RenderSystem(surface, factory(surface), menu_factory(surface))
+        return RenderSystem(surface, factory(surface))
